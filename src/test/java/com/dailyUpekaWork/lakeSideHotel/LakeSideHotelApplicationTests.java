5cf134e4dbe3f31cf99993487750d5eb61f6df20
pauponzdev <pasindu.upeka@onzdev.com>
2024-07-31 21:16:39 +0530

    first commit

---
+++ src/test/java/com/dailyUpekaWork/lakeSideHotel/LakeSideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyUpekaWork.lakeSideHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeSideHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
